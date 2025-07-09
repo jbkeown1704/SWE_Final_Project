4cd45818039faece479dfbab57e51ebd97d2a29f
James Keown <jkeown07@qub.ac.uk>
2025-07-09 21:18:49 +0100

    Dashboard trying to get the React to work

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 function Dashboard() {
-    return (
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Welcome to SPES</h1>
-      <p>You are now logged in.</p>
-    </div>
-    );
+  return (
+    <div className="dashboard-cards">
+  <div className="login-container">
+    {/* box 1 */}
+  </div>
+  <div className="login-container">
+    {/* box 2*/}
+  </div>
+  <div className="login-container">
+    {/* box 3 */}
+  </div>
+</div>
+
+  );
 }
+
 
 export default Dashboard;
